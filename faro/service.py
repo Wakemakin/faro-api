7caea49011f104b789eab81c6685db4334a9d864
Justin Hammond <justin.hammond@rackspace.com>
2013-06-26 01:40:48 +0000

    basic restless api

---
+++ faro/service.py
@@ -0,0 +1,20 @@
+import flask
+import flask.ext.sqlalchemy
+import flask.ext.restless
+
+from db import models as db
+from utils import common
+
+
+app = common.make_json_app(__name__)
+app.config['DEBUG'] = True
+
+mysession = db.make_session()
+
+manager = flask.ext.restless.APIManager(app, session=mysession)
+
+manager.create_api(db.User, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(db.Event, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(db.Choice, methods=['GET', 'POST', 'DELETE'])
+
+app.run(host='0.0.0.0')
