503a053653d70b7595f53f5c84f18c453a63d671
Justin Hammond <justin.hammond@rackspace.com>
2013-07-09 21:35:22 +0000

    Updated favicon

--- faro_api/views/endpoint.py
+++ faro_api/views/endpoint.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Blueprint
+from flask import Blueprint, Response
 
 
 mod = Blueprint('endpoint', __name__)
@@ -10,3 +10,8 @@ logger = logging.getLogger('faro_api.'+__name__)
 @mod.route('/', methods=['GET'])
 def index():
     return 'The greats were great cause they paint a lot'
+
+
+@mod.route('/favicon.ico', methods=['GET'])
+def favicon():
+    return Response(status=204)
