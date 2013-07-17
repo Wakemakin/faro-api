b25b043b23db4bf5ed3f9d88416ffab7cd64a127
Justin Hammond <justin.hammond@rackspace.com>
2013-07-17 23:24:28 +0000

    Added cross domain support through decorator

--- faro_api/utils.py
+++ faro_api/utils.py
@@ -1,9 +1,11 @@
+import datetime
 import json
 import re
 import uuid
 
 import flask
 import flask.ext.jsonpify as jsonp
+import functools
 import werkzeug.exceptions as http
 
 from faro_api.exceptions import common as exc
@@ -76,3 +78,45 @@ def make_json_app(app, **kwargs):
         app.error_handler_spec[None][code] = make_json_error
 
     return app
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, datetime.timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = flask.current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and flask.request.method == 'OPTIONS':
+                resp = flask.current_app.make_default_options_response()
+            else:
+                resp = flask.make_response(f(*args, **kwargs))
+            if not attach_to_all and flask.request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return functools.update_wrapper(wrapped_function, f)
+    return decorator
